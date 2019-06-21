f88a430ee17e53598aa7d4f5b7e391f7caef0b9d
Rob Thijssen <rthijssen@gmail.com>
2019-06-21 19:04:21 +0300

    extract last page no

--- src/github.rs
+++ src/github.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 //use serde_json::{Value};
+use regex::Regex;
 use restson::{RestPath,Error};
 
 pub const API_URL: &str = "https://api.github.com";
@@ -77,4 +78,43 @@ impl RestPath<String> for Activity {
   fn get_path(github_username: String) -> Result<String,Error> {
     Ok(format!("users/{}/events", github_username))
   }
+}
+
+fn extract_page(input: &str) -> Option<&u32> {
+  lazy_static! {
+    static ref RE: Regex = Regex::new(r"page=([0-9]*)").unwrap();
+  }
+  RE.captures(input).and_then(|cap| {
+    cap.name("page").map(|page| page.parse::<u32>())
+  })
+}
+
+pub fn get_last_page_number(url: String) -> u32 {
+  let response = reqwest::get(&url).expect("failed to send request");
+  if response.status().is_success() {
+    println!("{}", response.status());
+
+
+    let last_page_link_header = response.headers().get(reqwest::header::LINK).unwrap().to_str().unwrap().split(",").last().unwrap()
+    let re = Regex::new(r"page=(?P<page>[0-9]*)").unwrap();
+    let cap = match re.captures(&last_page_link_header) {
+      Some(data) => data,
+      None => panic!("failed to match page number in link header")
+    };
+    let page = match cap.name("idno") {
+      Some(data) => Some(match data.parse::<u32>() {
+        Ok(data) => data,
+        Err(err) => panic!("died in u32 parse")
+      }),
+      None => None
+    };
+    println!("{:?}", extract_page());
+    return 1;
+  } else {
+    return 0;
+  }
+  //for header in response.headers().iter() {
+    //println!("{}: {}", header.name(), header.value_string());
+  //  println!("{:?}", header);
+  //}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate chrono;
 #[macro_use]
+extern crate lazy_static;
+extern crate regex;
+extern crate reqwest;
+extern crate restson;
+#[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 extern crate serde;
-extern crate restson;
 extern crate yaml_rust;
 
 use restson::{RestClient};
@@ -25,6 +29,10 @@ fn main() {
   let mut github_api = RestClient::new(github::API_URL).unwrap();
   let query = vec![("page", "2")];
   for github_username in config["github"]["usernames"].clone() {
+    //let url = format!("{}/{}", github::API_URL, github::Activity::get_path(env::args().nth(1).unwrap()).unwrap());
+    //let url = format!("{}/users/grenade/events", github::API_URL);
+    github::get_last_page_number("https://api.github.com/users/grenade/events".to_string());
+
     let github_activity: github::Activity = github_api.get_with(github_username.as_str().unwrap().to_string(), &query).unwrap();
     println!("{:?}", github_activity);
   }
