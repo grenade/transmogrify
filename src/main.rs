80a99cfb86b702499176ec1e8a4a34c63b46bf05
Rob Thijssen <rthijssen@gmail.com>
2019-06-21 17:40:14 +0300

    read username from config rather than command args

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate serde;
 extern crate restson;
+extern crate yaml_rust;
+
 use restson::{RestClient};
-use std::env;
+use std::{
+  fs,
+  io::prelude::*
+};
 
 mod github;
 
 // debug usage:
-// RUST_BACKTRACE=1 cargo run -- grenade
+// RUST_BACKTRACE=1 cargo run
 fn main() {
+  let mut config_file = fs::File::open("config.yml").expect("unable to open config file");
+  let mut config_text = String::new();
+  config_file.read_to_string(&mut config_text).expect("unable to read config file");
+  let config = &yaml_rust::YamlLoader::load_from_str(&config_text).unwrap()[0];
+
   let mut github_api = RestClient::new(github::API_URL).unwrap();
   let query = vec![("page", "2")];
-  let github_activity: github::Activity = github_api.get_with(env::args().nth(1).unwrap(), &query).unwrap();
-  println!("{:?}", github_activity);
+  for github_username in config["github"]["usernames"].clone() {
+    let github_activity: github::Activity = github_api.get_with(github_username.as_str().unwrap().to_string(), &query).unwrap();
+    println!("{:?}", github_activity);
+  }
 }
